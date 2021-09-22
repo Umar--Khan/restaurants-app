e9ff7a99f69bd89384f5d9134355a10df2079193
Umar--Khan <umarkhandev@gmail.com>
2021-09-22 23:32:03 +0100

    test: added tests for restaurants - addresscard usercontext

--- src/components/AddressCard/AddressCard.tsx
+++ src/components/AddressCard/AddressCard.tsx
@@ -1,19 +1,25 @@
+import { useContext } from 'react';
+
 import TextLink from 'shared/TextLink/TextLink';
 import Card from 'shared/Card/Card';
+import { UserContext } from 'context/userContext';
 
 import { ReactComponent as MapPin } from 'assets/icons/map-pin.svg';
 
-const AddressCard = () => (
-  <Card className='address-card'>
-    <span className='address-card__icon'>
-      <MapPin />
-    </span>
-    <div className='address-card__text'>
-      {/* TODO: Use address details from context api user details */}
-      <p>BS8 4ES, Leigh woods</p>
-      <TextLink href='/'>Change location</TextLink>
-    </div>
-  </Card>
-);
+const AddressCard = () => {
+  const { location } = useContext(UserContext);
+
+  return (
+    <Card className='address-card'>
+      <span className='address-card__icon'>
+        <MapPin />
+      </span>
+      <div className='address-card__text'>
+        <p>{`${location.postcode}, ${location.firstLine}`}</p>
+        <TextLink href='/'>Change location</TextLink>
+      </div>
+    </Card>
+  );
+};
 
 export default AddressCard;
